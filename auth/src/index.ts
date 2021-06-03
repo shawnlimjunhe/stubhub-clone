51202d91c57c0a25b597ecdc61d049bae2ef6e2d
shawnlimjunhe <46809501+shawnlimjunhe@users.noreply.github.com>
2021-06-03 11:32:31 +0800

    Adding mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -24,6 +24,21 @@ app.all('*', async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log('Listening on port 3000!!');
-});
+const start = async () => {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+    console.log('Connected to mongoDB');
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!');
+  });
+};
+
+start();
